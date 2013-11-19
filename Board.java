f3e49cc3df79118d3ac9a6eff68a27aff70b3ad9
Andrew Berger <andbberger@gmail.com>
2013-11-19 01:48:23 -0800

    Good progress in board. wrote a bunch of tables for rays and stuff.

--- Board.java
+++ Board.java
@@ -20,9 +20,18 @@ class Board {
             clearRank[i] = ~maskRank[i];
             clearFile[i] = ~maskFile[i];
         }
+        north = new long[64];
+        northEa = new long[64];
+        west = new long[64];
+        southWe = new long[64];
+        south = new long[64];
+        southEa = new long[64];
+        east = new long[64];
+        northWe = new long[64];
+        initRays();
     }
-
-
+    
+    
     /** Returns an array of longs representing the board state.
      *  Each long treated as a 64 bit word 
      *  Bit indexing: 
@@ -128,8 +137,58 @@ class Board {
      *  soWe         sout         soEa
      *
      **/
-    public static long ray(long start, int orientation) {
-        
+    public static long ray(int square, int orientation) {
+        switch (orientation) {
+        case (8):
+            return north[square];
+        case (9):
+            return northEa[square];
+        case (1):
+            return east[square];
+        case (-7):
+            return southEa[square];
+        case(-8):
+            return south[square];
+        case(-9):
+            return southWe[square];
+        case(-1):
+            return west[square];
+        case (7):
+            return northWe[square];
+        default:
+            throw new Exception("Orientation is not an allowed value");
+        }
+    }
+
+    /** Called by the constructor. Fills in our ray tables */
+    private void initRays() {
+        for (int rank = 0; rank < 8; rank++) {
+            for (int file = 0; file < 8; file++) {
+                int ind = rank * 8 + file;
+                long vertical = maskFile[file] & clearRank[rank];
+                long horizontal = maskRank[rank] & clearFile[rank];
+                north[ind] = rankRange(rank, 7) & vertical;
+                south[ind] = rankRange(0, rank) & vertical;
+                west[ind] = fileRank(0, file) & horizontal;
+                east[ind] = fileRank(file, 7) & horizontal;
+            }
+        }
+        for (int i = 0; i < 64; i++) {
+            northEa[i] = (1 << i);
+            northWe[i] = (1 << i);
+            southEa[i] = (1 << i);
+            southWe[i] = (1 << i);
+            for (int j = 0; j < 8; j++) {
+                northEa[i] |= (northEa[i] & clearFile[7]) << 9;
+                northWe[i] |= (northWe[i] & clearFile[0]) << 7;
+                southEa[i] |= (southEa[i] & clearFile[7]) << -7;
+                southWe[i] |= (southWe[i] & clearFile[0]) << -9;
+            }
+            northEa[i] &= ~(1 << i);
+            northWe[i] &= ~(1 << i);
+            southEa[i] &= ~(1 << i);
+            southWe[i] &= ~(1 << i);
+        }
     }
     
     /** Ranks correspond to numbers. RANK corresponds to letters
@@ -152,6 +211,24 @@ class Board {
         return result;
     }
 
+    /** Returns state filled from rank [start to end) */ 
+    private static long rankRange(int start, int end) {
+        long result = 0;
+        for (int i = start; i < end; i++) {
+            result |= fillRank(i);
+        }
+        return result;
+    }
+
+    /** Returns state filled from file [start to end) */ 
+    private static long rankFile(int start, int end) {
+        long result = 0;
+        for (int i = start; i < end; i++) {
+            result |= fillFile(i);
+        }
+        return result;
+    }
+
     /** Each of the following four tables is of size 8.
      *  The entry at i performs the eponymous operation when &ed with a state*/
     private static final long[] clearRank;
@@ -159,5 +236,14 @@ class Board {
     private static final long[] maskRank;
     private static final long[] maskFile;
 
+    /** Each entry is the eponymous ray for that square */
+    private static final long[] north;
+    private static final long[] northEa;
+    private static final long[] west;
+    private static final long[] southWe;
+    private static final long[] south;
+    private static final long[] southEa;
+    private static final long[] east;
+    private static final long[] northWe;
 }
     
